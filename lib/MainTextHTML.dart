d9783ce9a0633616a6997088ad31a2794311e4e8
Alexy Marcell <alexy.marcell@hotmail.com>
2019-03-27 20:38:41 +0100

    DOM walking 1

--- lib/MainTextHTML.dart
+++ lib/MainTextHTML.dart
@@ -5,4 +5,54 @@ library MainTextHTML;
 
 export 'src/MainTextHTML_base.dart';
 
-// TODO: Export any libraries intended for clients of this package.
+import 'dart:core';
+import 'dart:io';
+import 'package:html/dom.dart' as HTML;
+import 'package:html/parser.dart' as HTMLParser;
+
+main() async {
+  var htmlFile = File('local/index.html');
+  var document = HTMLParser.parse(await htmlFile.readAsBytes());
+  _walkNodes(document);
+}
+
+
+Map<HTML.Node, double> _storedScores;
+
+/// Rules for scoring:
+///  - +1 point for every inner character
+///  - +90% of each child's score
+double score(HTML.Node node) {
+  if(_storedScores.containsKey(node)) return _storedScores[node];
+  var s = 0.0; // score
+  s += node.text.length;
+  for (final child in node.nodes) {
+    s += 0.9 * score(child);
+  }
+  return s;
+}
+
+const Map<int, String> _nodeTypeName = {
+  HTML.Node.ATTRIBUTE_NODE: 'ATTRIBUTTE_NODE',
+  HTML.Node.CDATA_SECTION_NODE: 'CDATA_SECTION_NODE',
+  HTML.Node.COMMENT_NODE: 'COMMENT_NODE',
+  HTML.Node.DOCUMENT_FRAGMENT_NODE: 'DOCUMENT_FRAGMENT_NODE',
+  HTML.Node.DOCUMENT_NODE: 'DOCUMENT_NODE',
+  HTML.Node.DOCUMENT_TYPE_NODE: 'DOCUMENT_TYPE_NODE',
+  HTML.Node.ELEMENT_NODE: 'ELEMENT_NODE',
+  HTML.Node.ENTITY_NODE: 'ENTITY_NODE',
+  HTML.Node.ENTITY_REFERENCE_NODE: 'ENTITY_REFERENCE_NODE',
+  HTML.Node.NOTATION_NODE: 'NOTATION_NODE',
+  HTML.Node.PROCESSING_INSTRUCTION_NODE: 'PROCESSING_INSTRUCTION_NODE',
+  HTML.Node.TEXT_NODE: 'TEXT_NODE',
+};
+int cnt = 0;
+_walkNodes(HTML.Node node, [int parent = 0]) {
+  int me = ++cnt;
+  //if(node.nodeType != HTML.Node.TEXT_NODE && node.nodeType != HTML.Node.ELEMENT_NODE)
+  print("${parent}>${me}\t${_nodeTypeName[node.nodeType]}: ${node.attributes}, ${node.text?.length}");
+  node.hasContent();
+  for(final child in node.nodes) {
+    _walkNodes(child, me);
+  }
+}
