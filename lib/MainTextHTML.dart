1e1006f385591a3bbcb69dfcf2b89566f5c1ee5a
Alexy Marcell <alexy.marcell@hotmail.com>
2019-03-30 12:15:24 +0100

    Added notes.

--- lib/MainTextHTML.dart
+++ lib/MainTextHTML.dart
@@ -10,9 +10,29 @@ import 'dart:io';
 import 'package:html/dom.dart' as HTML;
 import 'package:html/parser.dart' as HTMLParser;
 
+/*
+Root is the document.
+It will have a ELEMENT_NODE with 'html' localname, (and maybe some other).
+Element nodes can have element children. Node list will have text nodes also.
+Text nodes will include every whitespace.
+ */
+
 main() async {
+  //*
   var htmlFile = File('local/index.html');
   var document = HTMLParser.parse(await htmlFile.readAsBytes());
+  // *///
+  /*
+  var htmlFile = '''
+  <div>
+    <span style=”font-size: 200%;”>H</span>
+
+    <span>ello<span>
+
+  </div>
+''';
+  var document = HTMLParser.parse(htmlFile);
+  */
   _walkNodes(document);
 }
 
@@ -20,16 +40,19 @@ main() async {
 Map<HTML.Node, double> _storedScores;
 
 /// Rules for scoring:
-///  - +1 point for every inner character
-///  - +90% of each child's score
+///  - +1 point for every inner character in TEXT_NODEs
+///  - +50% of each child's score
 double score(HTML.Node node) {
   if(_storedScores.containsKey(node)) return _storedScores[node];
   var s = 0.0; // score
-  s += node.text.length;
-  for (final child in node.nodes) {
-    s += 0.9 * score(child);
+  if(node.nodeType == HTML.Node.TEXT_NODE) {
+    s += node.text.length;
   }
-  return s;
+  for (final child in node.nodes) {
+    s += 0.5 * score(child);
+  }
+
+  return _storedScores[node] = s;
 }
 
 const Map<int, String> _nodeTypeName = {
@@ -50,7 +73,10 @@ int cnt = 0;
 _walkNodes(HTML.Node node, [int parent = 0]) {
   int me = ++cnt;
   //if(node.nodeType != HTML.Node.TEXT_NODE && node.nodeType != HTML.Node.ELEMENT_NODE)
-  print("${parent}>${me}\t${_nodeTypeName[node.nodeType]}: ${node.attributes}, ${node.text?.length}");
+  if(node is HTML.Element) {
+    print('${node.localName}');
+  }
+  print('${parent}>${me}\t${_nodeTypeName[node.nodeType]}: ${node.attributes}, ${node.text?.length} [${node.text}]');
   node.hasContent();
   for(final child in node.nodes) {
     _walkNodes(child, me);
